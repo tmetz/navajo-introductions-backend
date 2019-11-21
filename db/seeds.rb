07a1740b83923362d898de0ef3e1c68aa9e85581
tmetz <tammy@tammymetz.com>
2019-11-20 23:32:44 -0800

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,144 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+ClanGroup.create(name: 'Group 1', color: '#ffffff') #white
+Clan.create(english_name: "Towering House", dine_bizaad_name: "Kinyaa'áanii", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Near The Mountain", dine_bizaad_name: "Dził t'aadi", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Big Medicine People", dine_bizaad_name: "'Azee'tsoh dine'é", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Turkey People", dine_bizaad_name: "Tązhii dine'é", clan_group_id: 1, count: 0)
+Clan.create(english_name: "People Of The Valley", dine_bizaad_name: "Halgai dine'é", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Bear People", dine_bizaad_name: "Shash dine'é", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Corn People", dine_bizaad_name: "Naadą́ą́' dine'é", clan_group_id: 1, count: 0)
+Clan.create(english_name: "Squash People", dine_bizaad_name: "Naayízí dine'é", clan_group_id: 1, count: 0)
+
+
+
+ClanGroup.create(name: 'Group 2', color: '#6d97b3') #grey/blue
+Clan.create(english_name: "One Who Walks Around", dine_bizaad_name: "Honágháahnii", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Near The Water", dine_bizaad_name: "Tó'áhani", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Tangle", dine_bizaad_name: "Ta'neeszahnii", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Yucca Fruit Strung Out", dine_bizaad_name: "Hashk'ąą hadzohi", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Gray Streaked Ends", dine_bizaad_name: "Nihoobáanii", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Sage Brush Hill", dine_bizaad_name: "Ts'ah yisk'idnii", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Mountain Cove", dine_bizaad_name: "Dził tł'ahnii", clan_group_id: 2, count: 0)
+Clan.create(english_name: "Turning Mountain", dine_bizaad_name: "Dził ná'oodiłnii", clan_group_id: 2, count: 0)
+
+
+ClanGroup.create(name: 'Group 3', color: '#ebdd44') #yellow...ish
+Clan.create(english_name: "Bitter Water", dine_bizaad_name: "Tódich'ii'nii", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Clamp Tree", dine_bizaad_name: "Tsin sikaadnii", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Bead People", dine_bizaad_name: "Yoo'í dine'é", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Deer Spring", dine_bizaad_name: "Bįįh bitoo'nii", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Zia Pueblo", dine_bizaad_name: "Tł'ógi dine'é", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Salt Water", dine_bizaad_name: "Tódik'ǫzhi", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Two Who Came To The Water", dine_bizaad_name: "Tó baazhni'ázhi", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Flat Foot People - Tohono O'odham", dine_bizaad_name: "Naakétł'áhí", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Little Deer People", dine_bizaad_name: "Bįįhyáázh dine'é", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Arrow People", dine_bizaad_name: "K'aa' dine'é", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Living Arrow", dine_bizaad_name: "K'aa' hinaanii", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Bead People of Bitter Water", dine_bizaad_name: "Yoo'í dine'é  Tódich'ii'nii", clan_group_id: 3, count: 0)
+Clan.create(english_name: "Crystal Rock", dine_bizaad_name: "Tséghá'dinínii dine'é", clan_group_id: 3, count: 0)
+
+
+ClanGroup.create(name: 'Group 4', color: '#000000') #black
+Clan.create(english_name: "Mud Clan", dine_bizaad_name: "Hashtł'ishnii", clan_group_id: 4, count: 0)
+Clan.create(english_name: "Big Water", dine_bizaad_name: "Tótsohnii", clan_group_id: 4, count: 0)
+Clan.create(english_name: "Many Hogans", dine_bizaad_name: "Hooghan łáni   ", clan_group_id: 4, count: 0)
+Clan.create(english_name: "Many Mules", dine_bizaad_name: "Dzaanééz łáni", clan_group_id: 4, count: 0)
+Clan.create(english_name: "Rock Gap", dine_bizaad_name: "Tsé deeshgizhnii", clan_group_id: 4, count: 0)
+Clan.create(english_name: "Reed People", dine_bizaad_name: "Lók'aa' dine'é", clan_group_id: 4, count: 0)
+
+
+ClanGroup.create(name: 'Group 5', color: '#266125') #dark green
+Clan.create(english_name: "Water Edge", dine_bizaad_name: "Tábąąhí", clan_group_id: 5, count: 0)
+Clan.create(english_name: "Meadow People", dine_bizaad_name: "Haltsooí", clan_group_id: 5, count: 0)
+Clan.create(english_name: "Two Who Came To The Water", dine_bizaad_name: "Tó baazhni'ázhí", clan_group_id: 5, count: 0)
+Clan.create(english_name: "Water Edge Corn", dine_bizaad_name: "Naadąą' dine'é Tábąąhí", clan_group_id: 5, count: 0)
+Clan.create(english_name: "Water Edge Zuni", dine_bizaad_name: "Naasht'ézhí dine'é Tábąąhí", clan_group_id: 5, count: 0)
+
+
+ClanGroup.create(name: 'Group 6', color: '#c91f16') #red
+Clan.create(english_name: "Red Running Into The Water", dine_bizaad_name: "Táchii'nii", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Tobacco People", dine_bizaad_name: "Nát'oh dine'é", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Giant People", dine_bizaad_name: "Yé'ii dine'é", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Blue Bird", dine_bizaad_name: "Dólii dine'é", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Red Goat", dine_bizaad_name: "Tł'izí daalchí'í", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Charchoal Streaked of the Red Running Into The Water", dine_bizaad_name: "Naaneesht'ézhí Táchii'nii", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Ute People of the Red Running Into The Water", dine_bizaad_name: "Nóóda'í dine'é Táchii'nii", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Zuni People", dine_bizaad_name: "Naasht'ezhí dine'é", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Deer People of the Red Running Into The Water", dine_bizaad_name: "Bįįh dine'é Táchii'nii", clan_group_id: 6, count: 0)
+Clan.create(english_name: "Rabbit People of the Red Running Into The Water", dine_bizaad_name: "Gah dine'é Táchii'nii", clan_group_id: 6, count: 0)
+
+
+ClanGroup.create(name: 'Group 7', color: '#c97516') #brown
+Clan.create(english_name: "Honey Combed Rock or Cliff Dwellers", dine_bizaad_name: "Tsé níjíkiní", clan_group_id: 7, count: 0)
+Clan.create(english_name: "Black Sheep", dine_bizaad_name: "Dibé łizhiní", clan_group_id: 7, count: 0)
+Clan.create(english_name: "Salt People", dine_bizaad_name: "Áshįįhí", clan_group_id: 7, count: 0)
+Clan.create(english_name: "Coyote Pass - Jemez", dine_bizaad_name: "Ma'ii deeshgiizhinii", clan_group_id: 7, count: 0)
+Clan.create(english_name: "Yellow House", dine_bizaad_name: "Kin łitsonii", clan_group_id: 7, count: 0)
+Clan.create(english_name: "Turning Mountain", dine_bizaad_name: "Dził ná'oodiłnii", clan_group_id: 7, count: 0)
+
+
+ClanGroup.create(name: 'Group 8', color: '#ff4500') #orange
+Clan.create(english_name: "The Water Flows Together", dine_bizaad_name: "Tó'aheedlíínii", clan_group_id: 8, count: 0)
+Clan.create(english_name: "Mexican People", dine_bizaad_name: "Naakai dine'é", clan_group_id: 8, count: 0)
+Clan.create(english_name: "Foot Trail People", dine_bizaad_name: "Kéha'atiinii ", clan_group_id: 8, count: 0)
+Clan.create(english_name: "Ute People", dine_bizaad_name: "Nóoda'í dine'é", clan_group_id: 8, count: 0)
+
+
+ClanGroup.create(name: 'Group 9', color: '#cea7d9') #lavender
+Clan.create(english_name: "Black Streak Wood People", dine_bizaad_name: "Tsi'naajinii", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Start At The Red Streak People", dine_bizaad_name: "Deeshchii'nii", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Red House", dine_bizaad_name: "Kin łichíí'nii ", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Red Bottom People", dine_bizaad_name: "Tł'ááshchí'í  ", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Many Goats", dine_bizaad_name: "Tł'ízí lání", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Sleep Rock People", dine_bizaad_name: "Tsé nabahiłnii", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Bear People", dine_bizaad_name: "Shash dine'é - Naashashí", clan_group_id: 9, count: 0)
+Clan.create(english_name: "Gray Cottonwood Extending Out", dine_bizaad_name: "T'iis ch'ébáanii", clan_group_id: 9, count: 0)
+
+
+ClanGroup.create(name: 'Group 10', color: '#8bc79b') #light green
+Clan.create(english_name: "Mescalero Apache", dine_bizaad_name: "Naashgali", clan_group_id: 10, count: 0)
+Clan.create(english_name: "Chiricahua Apache", dine_bizaad_name: "chishí", clan_group_id: 10, count: 0)
+Clan.create(english_name: "White Mountain Apache", dine_bizaad_name: "Dził ghą́'ą́", clan_group_id: 10, count: 0)
+Clan.create(english_name: "Jicarilla Apache", dine_bizaad_name: "Beehai", clan_group_id: 10, count: 0)
+
+
+ClanGroup.create(name: 'Group 11', color: '#8bc79b') #light green
+Clan.create(english_name: "Hopi - Polacca", dine_bizaad_name: "Ayah Kinii dine'é", clan_group_id: 11, count: 0)
+Clan.create(english_name: "Hopi - Oraibi", dine_bizaad_name: "Ooséí", clan_group_id: 11, count: 0)
+
+
+ClanGroup.create(name: 'Group 12', color: '#8bc79b') #light green
+Clan.create(english_name: "House Made of Sand", dine_bizaad_name: "Séíbeehoghanii", clan_group_id: 12, count: 0)
+
+
+ClanGroup.create(name: 'Group 13', color: '#8bc79b') #light green
+Clan.create(english_name: "Big Deer People", dine_bizaad_name: "Bįįhtsoh dine'é", clan_group_id: 13, count: 0)
+Clan.create(english_name: "The Sticking Up Ears People", dine_bizaad_name: "Jaa'yaalóolii dine'é", clan_group_id: 13, count: 0)
+Clan.create(english_name: "The Light Water People", dine_bizaad_name: "Tó'aszóli dine'é", clan_group_id: 13, count: 0)
+Clan.create(english_name: "Tree Stretcher - Carries With Wooden Stick People", dine_bizaad_name: "Tsin yeena'alo'íí dine'é", clan_group_id: 13, count: 0)
+
+
+ClanGroup.create(name: 'Group 14', color: '#442163') #purple
+Clan.create(english_name: "Anglo - White People", dine_bizaad_name: "Bilagáana", clan_group_id: 14, count: 0)
+Clan.create(english_name: "German", dine_bizaad_name: "Bééshbich'ahii", clan_group_id: 14, count: 0)
+Clan.create(english_name: "Italian", dine_bizaad_name: "Doohats'yáłti'í", clan_group_id: 14, count: 0)
+Clan.create(english_name: "Dutch", dine_bizaad_name: "Kétsiní", clan_group_id: 14, count: 0)
+
+
+ClanGroup.create(name: 'Group 15', color: '#5f5469') #grey
+Clan.create(english_name: "Black People", dine_bizaad_name: "Naakaii łizhinii", clan_group_id: 15, count: 0)
+
+ClanGroup.create(name: 'Group 16', color: '#d145a5') #pink
+Clan.create(english_name: "Japanese", dine_bizaad_name: "Bináá'adaałtsó'zí", clan_group_id: 16, count: 0)
+Clan.create(english_name: "Chinese", dine_bizaad_name: "Bitsii'yisbizhii", clan_group_id: 16, count: 0)
+
+# EXTINCT:
+# Salt People - Ashiinii
+# Bit'aanii
+# Moth People
+# Tseyanatohnii
