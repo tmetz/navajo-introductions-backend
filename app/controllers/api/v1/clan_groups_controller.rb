1472eb1568100744c7516919f5aea0347a2a1511
tmetz <tammy@tammymetz.com>
2019-11-25 20:19:45 -0800

    create show action for clan groups controller

--- app/controllers/api/v1/clan_groups_controller.rb
+++ app/controllers/api/v1/clan_groups_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::ClanGroupsController < ApplicationController
 
     def index
-        @groups = ClanGroup.all
+        groups = ClanGroup.all
         
-        render json: @groups, status: 200
+        render json: groups, status: 200
+    end
+
+    def show
+        group = ClanGroup.find(params[:id])
+
+        render json: group, include: [:clans]
     end
 end
